b7e34d3edeae095c5720603a1b20facdbc95a537
Selga P. Muiznieks <selga.muiznieks@gmail.com>
2022-05-10 20:01:33 -0400

    lowdb setup

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 // console.log(process.argv);
-const express = require('express');
+import express from "express";
 // console.log(express);
+import { join, dirname } from "path";
+import { Low, JSONFile } from "lowdb";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3001;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Use JSON file for storage
+const file = join(__dirname, "db.json");
+const adapter = new JSONFile(file);
+const db = new Low(adapter);
+
+// Read data from JSON file, this will set db.data content
+await db.read();
 
 app.get("/", (req, res) => {
   res.json({"name": "selga"});
